e1066706dab6d9c4c83b003bd39999723da128c4
gilanggsb <gilanggsb@gmail.com>
2024-12-16 10:54:01 +0700

    fix(getx_clean_arch_feature): invalid import in pages

--- bricks/getx_clean_arch_feature/__brick__/{{name.snakeCase()}}/views/pages/{{name.snakeCase()}}_page.dart
+++ bricks/getx_clean_arch_feature/__brick__/{{name.snakeCase()}}/views/pages/{{name.snakeCase()}}_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../{{name.snakeCase()}}.dart';
+import '../../{{name.snakeCase()}}.dart';
 
 class {{name.pascalCase()}}Page extends GetView<{{name.pascalCase()}}Controller> {
   const {{name.pascalCase()}}Page({super.key});
