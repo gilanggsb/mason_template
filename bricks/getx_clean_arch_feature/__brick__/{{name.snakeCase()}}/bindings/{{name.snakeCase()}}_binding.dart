17eb5fb96a301ee3987b7bbffb570482ea8c5e9b
gilanggsb <gilanggsb@gmail.com>
2024-12-17 11:20:52 +0700

    fix(getx_clean_arch_feature): name parameter in Bindings not based on feature name

--- bricks/getx_clean_arch_feature/__brick__/{{name.snakeCase()}}/bindings/{{name.snakeCase()}}_binding.dart
+++ bricks/getx_clean_arch_feature/__brick__/{{name.snakeCase()}}/bindings/{{name.snakeCase()}}_binding.dart
@@ -6,6 +6,6 @@ class {{name.pascalCase()}}Binding extends Bindings{
   @override
   void dependencies() {
     // Get.lazyPut(() => {{name.pascalCase()}}Repository(apiService: Get.find()));
-    Get.lazyPut(() => {{name.pascalCase()}}Controller(assignJobRepository: Get.find()));
+    Get.lazyPut(() => {{name.pascalCase()}}Controller({{name.camelCase()}}Repository: Get.find()));
   }
 }
